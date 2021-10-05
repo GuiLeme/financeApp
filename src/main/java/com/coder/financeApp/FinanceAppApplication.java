944788f61f510dfd2b0475741a769ce16e0498ac
GuiLeme <leme.guilherme.p@gmail.com>
2021-10-04 22:44:27 -0300

    First commit: creating Spring project

---
+++ src/main/java/com/coder/financeApp/FinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.coder.financeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coder/financeApp/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.financeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
